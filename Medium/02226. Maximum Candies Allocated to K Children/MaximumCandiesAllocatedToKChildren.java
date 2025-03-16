a58819b050b3d96ee7e9179ae664e89b31812229
Trương Lê Anh Vũ <truongleanhvu20052004@gmail.com>
2025-03-16 17:01:49 +0700

    Update: 10 problem(s) remaining in need-review.txt

---
+++ Medium/02226. Maximum Candies Allocated to K Children/MaximumCandiesAllocatedToKChildren.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maximumCandies(int[] candies, long k) {
+        
+    }
+}
+
+public class MaximumCandiesAllocatedToKChildren {
+    public static void main(String[] args) {
+        
+    }
+}
